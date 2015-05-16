acb8e70b11abd2e42801cf79d883eb487f5b484d
samuraix <ivodwvio@mail.bg>
2015-05-16 17:15:59 +0300

    Initial commit

---
+++ examples/HiWorld.java
@@ -0,0 +1,11 @@
+package examples;
+
+class HiWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hi World!");
+		
+	}
+	
+}
---
+++ examples/PayIvailo.java
@@ -0,0 +1,16 @@
+package examples;
+
+public class PayIvailo {
+
+	public static void main(String[] args) {
+	
+		double checkAmount = 1257.63;
+		
+		System.out.print("Pay to the order of");
+		System.out.print(" Dr. Ivailo Kazandjiev");
+		System.out.print(" $");
+		System.out.println(checkAmount);
+		
+	}
+	
+}
